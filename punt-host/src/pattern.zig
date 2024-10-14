98688cf18916c9a6960827cc3c6765dd2fcb2142
Race Williams <race@racewilliams.com>
2024-10-14 15:43:18 -0400

    started, switching to laptop

--- punt-host/src/pattern.zig
+++ punt-host/src/pattern.zig
@@ -1,12 +1,56 @@
+/// Pattern is the software side of our internal represenation of orders.
+/// We have three parts:
+///     - a one bit flag for buy/sell
+///     - q bits for the quantity
+///     - p bits for the limit price
+///
+/// The position of the decimal within q and p is implied.
+///
+/// With this struct defined, you'll still need to manually update other componenets
+/// to match the pattern bit width. Consider this Verilog:
+/// ```verilog
+/// module rule_parser #(
+///     parameter PRICE_BITS = 10
+///     parameter QUANTITY_BITS = 9,
+///     parameter TOTAL_BITS = 1 + PRICE_BITS + QUANTITY_BITS
+/// ) (
+///     input wire [TOTAL_BITS-1:0] rule_data,
+///     output wire buy_sell,
+///     output wire [PRICE_BITS-1:0] price,
+///     output wire [QUANTITY_BITS-1:0] quantity
+/// );
+///     assign buy_sell = rule_data[TOTAL_BITS-1];
+///     assign price = rule_data[TOTAL_BITS-2 -: PRICE_BITS];
+///     assign quantity = rule_data[QUANTITY_BITS-1:0];
+/// endmodule
+/// ```
 const std = @import("std");
 
-// not sure if you can use `comptime` to set these bits dynamically
-pub fn Pattern(comptime PRICE_BITS: , comptime QUANTITY_BITS: comptime_int) type {
-    // the 1 is the buy/sell flag
-    const TOTAL_BITS = @as(u6, 1 + PRICE_BITS + QUANTITY_BITS);
+pub const PatternError = error{
+    BufferTooSmall,
+};
 
-    const DataType = switch (TOTAL_BITS) {
+pub fn Pattern() type {
+    // The packed struct for order data.
+    return packed struct {
+        isBuyOrder: bool, // otherwise, is Sell
+        limitPrice: u15, // counted in mills (.1 cents, .001 usd)
+        quantity: u16,
 
-    }
+        pub fn new(
+            isBuyOrder: bool,
+            limitPrice: u15,
+            quantity: u16,
+        ) !*const Pattern {
+            return new_cast(isBuyOrder, limitPrice, quantity);
+        }
+
+        pub fn new_cast(buyIfHigh: u1, comptime quantity: comptime_int, price: comptime_int) !*const Pattern {
+            return Pattern{
+                .buyIfHigh = buyIfHigh,
+                .qBits = @intCast(quantity),
+                .pbits = @intCast(price),
+            };
+        }
+    };
 }
-// see what I'm trying to do though? see discord thread on the q/p stuff
